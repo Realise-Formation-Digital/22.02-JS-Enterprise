4fc6e089842e83c194b8eae1406c4291e1e92131
Sam <sami_kherbache@hotmail.com>
2022-04-28 11:15:50 +0200

    Fist Commit + page/navbar

---
+++ Ville.js
@@ -0,0 +1,87 @@
+/**
+ * 
+ * @returns data, base de donnee des villes;
+ */
+async function getVille() {
+    try {
+      const response = await axios.get('https://entreprise.vinko-roditi.com/api/ville/list');
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+}
+
+//array avec toutes les ville
+const dataVille = await getVille();
+
+/**
+ * 
+ * @param {Une ville} value 
+ * @returns Une card pour chaque ville dans dataVille.
+ */
+function CreateVille(value) {
+
+    let div0 = document.createElement("div");
+    div0.className= "card col-5";
+
+    let div1 = document.createElement("div");
+    div1.className = "card-header text-center";
+    
+    let h2 = document.createElement("h2");
+    h2.className = "fw-bold text-decoration-underline";
+    h2.innerHTML = value.nom;
+    div1.appendChild(h2);
+    div0.appendChild(div1);
+
+    let div2 = document.createElement("div");
+    div2.className= "card-body";
+
+    let div3 = document.createElement("div");
+    div3.className= "card-text mb-3";
+    div2.appendChild(div3);
+    div0.appendChild(div2);
+
+    let p1 = document.createElement("p");
+    p1.className = "text-justify fw-bold ms-2"; 
+    p1.innerHTML = "Lattitude: " + value.lattitude;
+
+    let br = document.createElement("br");
+    p1.appendChild(br);
+    div3.appendChild(p1);
+
+    let p2 = document.createElement("p");
+    p2.className = "text-justify fw-bold ms-2"; 
+    p2.innerHTML = "Longitude: " + value.longitude;
+
+    let br1 = document.createElement("br");
+    p2.appendChild(br1);
+    div3.appendChild(p2);
+
+    let p3 = document.createElement("p");
+    p3.className = "text-justify fw-bold ms-2"; 
+    p3.innerHTML = "Pays: " + value.pays;
+
+    let br2 = document.createElement("br");
+    p3.appendChild(br2);
+    div3.appendChild(p3);
+    div0.appendChild(div3);
+    
+    let div4 = document.createElement("div");
+    div4.className= "card-footer d-flex justify-content-center";
+    
+    //creation du button pour le modal
+    let button = document.createElement("button");
+    button.className = "btn btn-primary";
+    button.textContent= "Subscribe!";
+    button.setAttribute("type", "button");
+    button.setAttribute("data-bs-toggle", "modal");
+    button.setAttribute("data-bs-target", "#modalButton");
+    button.setAttribute("data-bs-value", value.id);
+    button.setAttribute("data-bs-array", "dataVille");
+    div4.appendChild(button);
+    div0.appendChild(div4);
+
+    return div0;
+}
+
+export { CreateVille, dataVille };
