a45106e7a20ffb24cb8af8caf53450f218f254d9
Elyes1290 <elyes.naitliman@gmail.com>
2022-05-01 13:59:11 +0200

    Version finale

--- www/Entreprise.js
+++ www/Entreprise.js
@@ -2,75 +2,99 @@
  * 
  * @returns data, base de donnee Entreprise;
  */
-async function getEntreprise() {
-    try {
-      const response = await axios.get('https://entreprise.vinko-roditi.com/api/entreprise/list');
-      return response.data;
-    } catch (error) {
-      alert(error);
-    }
+ async function getEntreprise() {
+  try {
+      const response = await axios.get('https://entreprise.vinko-roditi.com/api/entreprise/list', { timeout: 60 });
+      return response;
+  } catch (error) {
+      if (error.code === "ECONNABORTED") {
+          return error.message
+      } else if (error.dueToNoInternetConnection) {
+          return error = "No Connection";
+      } else {
+          return error.message;
+      }
+  }
 }
 
-//array avec toutes les entreprise;
-const dataEntreprise = await getEntreprise();
+//toutes les donnee de la requete
+const reponseAxios = await getEntreprise();
 
 /**
- * 
- * @param {Une Entreprise} value 
- * @returns Une card pour chaque entreprise dans dataEntreprise
- */
-function CreateEntreprise(value) {
-
-    let div0 = document.createElement("div");
-    div0.className= "card col-5";
-
-    let div1 = document.createElement("div");
-    div1.className = "card-header text-center";
-    
-    let h2 = document.createElement("h2");
-    h2.className = "fw-bold text-decoration-underline";
-    h2.innerHTML = value.nom;
-    div1.appendChild(h2);
-    div0.appendChild(div1);
-
-    let div2 = document.createElement("div");
-    div2.className= "card-body";
-
-    let div3 = document.createElement("div");
-    div3.className= "card-text mb-3";
-    div2.appendChild(div3);
-    div0.appendChild(div2);
-
-    let p1 = document.createElement("p");
-    p1.className = "text-justify fw-bold ms-2"; 
-    p1.innerHTML = "Telephone: " + value.telephone;
-    
-    let br = document.createElement("br");
-    p1.appendChild(br);
-    div3.appendChild(p1);
-
-    let a1 = document.createElement("a");
-    a1.className = "text-justify fw-bold ms-2";
-    a1.setAttribute("href", value.website);
-    a1.innerHTML = value.website;
-    div3.appendChild(a1);
-    div0.appendChild(div3);
-    
-    let div4 = document.createElement("div");
-    div4.className= "card-footer d-flex justify-content-center";
-    
-    let button = document.createElement("button");
-    button.className = "btn btn-primary";
-    button.textContent= "Subscribe!";
-    button.setAttribute("type", "button");
-    button.setAttribute("data-bs-toggle", "modal");
-    button.setAttribute("data-bs-target", "#modalButton");
-    button.setAttribute("data-bs-value", value.id);
-    button.setAttribute("data-bs-array", "dataEntreprise");
-    div4.appendChild(button);
-    div0.appendChild(div4);
-
-    return div0;
+* 
+* @returns liste d'entreprise, check le status 200
+*/
+function check () {
+  if (reponseAxios.status == 200) {
+      const data = reponseAxios.data;
+      console.log(data)
+      return data;
+  }
 }
 
-export { CreateEntreprise, dataEntreprise };
+//liste d'entreprise
+const dataEntreprise = check();
+
+//error de axios
+const errorEntreprise = reponseAxios;
+
+/**
+* 
+* @param {Une Entreprise} value 
+* @returns Une card pour chaque entreprise dans dataEntreprise
+*/
+function CreateEntreprise(value) {
+
+  let div0 = document.createElement("div");
+  div0.className= "card col-5";
+
+  let div1 = document.createElement("div");
+  div1.className = "card-header text-center";
+  
+  let h2 = document.createElement("h2");
+  h2.className = "fw-bold text-decoration-underline";
+  h2.innerHTML = value.nom;
+  div1.appendChild(h2);
+  div0.appendChild(div1);
+
+  let div2 = document.createElement("div");
+  div2.className= "card-body";
+
+  let div3 = document.createElement("div");
+  div3.className= "card-text mb-3";
+  div2.appendChild(div3);
+  div0.appendChild(div2);
+
+  let p1 = document.createElement("p");
+  p1.className = "text-justify fw-bold ms-2"; 
+  p1.innerHTML = "Telephone: " + value.telephone;
+  
+  let br = document.createElement("br");
+  p1.appendChild(br);
+  div3.appendChild(p1);
+
+  let a1 = document.createElement("a");
+  a1.className = "text-justify fw-bold ms-2";
+  a1.setAttribute("href", value.website);
+  a1.innerHTML = value.website;
+  div3.appendChild(a1);
+  div0.appendChild(div3);
+  
+  let div4 = document.createElement("div");
+  div4.className= "card-footer d-flex justify-content-center";
+  
+  let button = document.createElement("button");
+  button.className = "btn btn-primary";
+  button.textContent= "Subscribe!";
+  button.setAttribute("type", "button");
+  button.setAttribute("data-bs-toggle", "modal");
+  button.setAttribute("data-bs-target", "#modalButton");
+  button.setAttribute("data-bs-value", value.id);
+  button.setAttribute("data-bs-array", "dataEntreprise");
+  div4.appendChild(button);
+  div0.appendChild(div4);
+
+  return div0;
+}
+
+export { CreateEntreprise, dataEntreprise, errorEntreprise };
--- www/index.js
+++ www/index.js
@@ -8,34 +8,58 @@ const div1 = document.querySelector("#displayEntreprise");
 const div2 = document.querySelector("#displayPersonne");
 const div3 = document.querySelector("#displayVille");
 
+//lien pour handling errors toasts
+const textToast = document.getElementById("textError");
+const toast = new bootstrap.Toast(document.querySelector('#errorToast'), {
+    autohide: false
+});
 
-    
+const textToast2 = document.getElementById("textError2");
+const toast2 = new bootstrap.Toast(document.querySelector('#errorToast2'), {
+    autohide: false
+});
 
+const textToast3 = document.getElementById("textError3");
+const toast3 = new bootstrap.Toast(document.querySelector('#errorToast3'), {
+    autohide: false
+});
 
-//boucle pour chaque entreprise, personne, ville et genere une card pour chaque avec les information
-for (let value of dataEntreprise){
-    div1.appendChild(CreateEntreprise(value));
+//check les error et sinon boucle pour chaque entreprise, personne, ville et genere une card pour chaque avec les information
+if(errorEntreprise.data == undefined || errorEntreprise.data == null) {
+    toast.show();
+    textToast.innerHTML = "Error: " + errorEntreprise; 
+} else { 
+    for (let value of dataEntreprise){
+        div1.appendChild(CreateEntreprise(value));
+    }
 }
 
-for (let value of dataVille){
-    div3.appendChild(CreateVille(value));
+if(errorVille.data == undefined || errorVille.data == null) {
+    toast2.show();
+    textToast2.innerHTML = "Error: " + errorVille; 
+} else { 
+    for (let value of dataVille){
+        div3.appendChild(CreateVille(value));
+    }
 }
 
-for (let value of dataPersonne){
-    div2.appendChild(CreatePersonne(value));
+if(errorPersonne.data == undefined || errorPersonne.data == null) {
+    toast3.show();
+    textToast3.innerHTML = "Error: " + errorPersonne; 
+} else { 
+    for (let value of dataPersonne){
+        div2.appendChild(CreatePersonne(value));
+    }
 }
 
 //lien modal dans le html
 let modal = document.getElementById("modalButton");
-console.log(modalButton);
 
 /**
  * addEventListener pour chaque button dans les card, prend les info du button au click pour faire un modal avec avec lo nom puis la map.
  */
 modal.addEventListener("show.bs.modal", (e) => {
     e.stopPropagation()
-    // modal.getElementById('modalButton').style.display = 'block';
-
     
 
     let buttons = e.relatedTarget;
@@ -48,29 +72,18 @@ modal.addEventListener("show.bs.modal", (e) => {
     let arrayButton = buttons.getAttribute("data-bs-array");
     console.log(arrayButton);
 
-    // let modaltitle = modal.querySelector("#title");
-    // let modalbody = modal.querySelector(".modal-body");
-    
-
-    var id1 = dataVille.find(element => element.id == idButton)
-        console.log(id1);
-
-
-        let longitude = id1.longitude;
-        let lattitude = id1.lattitude;
-
-        console.log(longitude);
-        console.log(lattitude);
-
     if(arrayButton == "dataEntreprise") {
         var id = dataEntreprise.find(element => element.id == idButton)
+        var id1 = dataVille.find(element => element.id == idButton)
+        let longitude = id1.longitude;
+        let lattitude = id1.lattitude;
 
         console.log(id);
 
         var container = L.DomUtil.get('map');
             if(container != null){
                 container._leaflet_id = null;
-            }
+            } 
     
         setTimeout(function(){
             map.invalidateSize();
@@ -83,16 +96,14 @@ modal.addEventListener("show.bs.modal", (e) => {
             L.marker([lattitude, longitude]).addTo(map)
             .bindPopup(id.nom)
             .openPopup();
+            }
         
-        // modaltitle.textContent = id.nom;
-    }
-        
-        else if (arrayButton == "dataPersonne" && id1.id == idButton) {
+        else if (arrayButton == "dataPersonne") {
+            var id = dataPersonne.find(element => element.id == idButton)
+            var id1 = dataVille.find(element => element.id == id.ville_id)
+            let longitude = id1.longitude;
+            let lattitude = id1.lattitude;
             
-            
-            console.log(id1);
-
-            console.log(idButton);
             
         var container = L.DomUtil.get('map');
             if(container != null){
@@ -110,7 +121,6 @@ modal.addEventListener("show.bs.modal", (e) => {
             L.marker([lattitude, longitude]).addTo(map)
             .bindPopup(id1.nom)
             .openPopup();
-        // modaltitle.textContent = id.nom;
 
     } else {
         const id = dataVille.find(element => element.id == idButton)
@@ -134,101 +144,5 @@ modal.addEventListener("show.bs.modal", (e) => {
             L.marker([lattitude, longitude]).addTo(map)
             .bindPopup(id.nom)
             .openPopup();
-
-        // if(idButton == '3'){
-
-
-        // // modaltitle.textContent = id.nom;
-        // }
-        // else if(idButton == '2'){
-
-
-        //     L.marker([46.51965350, 6.63227340]).addTo(map)
-        //     .bindPopup('Lausanne')
-        //     .openPopup();
-        // // modaltitle.textContent = id.nom;
-        // }
-
-
-        
-        function effacer(){
-            map.off();;
-        }
-
-        
-
-        // if(id == "3"){
-        // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        //     }).addTo(map);
-
-        // var map = L.map('map').setView([-34.61315000, -58.37723000]);
-        // L.marker([-34.61315000, -58.37723000]).addTo(map)
-        //     .bindPopup('Buenos Aires')
-        // }
-
-        // else if(id == "2"){
-        //     var map1 = L.map('map').setView([46.51965350,6.63227340], 13);
-        //     L.marker([46.51965350,6.63227340]).addTo(map1)
-        //         .bindPopup('Genève')
-        // }
-        // else if(id == "1"){
-        //     var map2 = L.map('map').setView([46.20700000,6.14234000], 13);
-        //     L.marker([46.20700000,6.14234000]).addTo(map2)
-        //         .bindPopup('Lausanne')
-        // }
-        // else if(id == "4"){
-        //     var map3 = L.map('map').setView([-33.30851000,-70.11405900], 13);
-        //     L.marker([-33.30851000,-70.11405900]).addTo(map3)
-        //         .bindPopup('Santiago de Chile')
-        // }
     }
 }  )
-
-    // if(idButton == 3){
-    //     var map = L.map('map').setView([-58.37723000, -34.61315000], 13);
-    
-    //     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //     }).addTo(map);
-    
-    //     L.marker([-57, -33]).addTo(map)
-    //         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    //         .openPopup();
-    // }
-
-    // else if(idButton == 2){
-    //     var map = L.map('map2').setView([6.63227340, 46.51965350], 13);
-    
-    //     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //     }).addTo(map);
-    
-    //     L.marker([-57, -33]).addTo(map)
-    //         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    //         .openPopup();
-    // }
-
-    // else if(idButton == 1){
-    //     var map = L.map('map1').setView([46.20700000, 6.14234000], 13);
-    
-    //     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //     }).addTo(map);
-    
-    //     L.marker([-57, -33]).addTo(map)
-    //         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    //         .openPopup();
-    // }
-
-    // else if(idButton == 4){
-    //     var map = L.map('map4').setView([-70.11405900, -33.30851000], 13);
-    
-    //     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //     }).addTo(map);
-    
-    //     L.marker([-57, -33]).addTo(map)
-    //         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    //         .openPopup();
-    // }
